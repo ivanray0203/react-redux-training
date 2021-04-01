0ebabe734db56f71baf706236ca5222ac0f8f3e9
Ivan Ray Antonio <ivanray0621@gmail.com>
2021-04-01 21:55:11 +0800

    Initial Update Data

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import users from "./app/redux/user/user.reducer"
 import items from "./app/redux/Item/Item.reducers"
 import RouteManager from "./app/RouteManager"
 import todos from "./app/redux/todo/Todo.reducer"
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css"
 
 // Setup Redux store with Thunks
 const reducers = combineReducers({ items, users, todos })
@@ -14,9 +16,11 @@ const store = createStore(reducers, applyMiddleware(thunk))
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <RouteManager />
-        </Provider>
+        <div className="container m-5">
+            <Provider store={store}>
+                <RouteManager />
+            </Provider>
+        </div>
     )
 }
 
--- src/app/pages/todo/TodoList.tsx
+++ src/app/pages/todo/TodoList.tsx
@@ -7,6 +7,7 @@ interface RootState {
     todos: any
 }
 const TodoList = ({ todos }: TodoProps) => {
+    const [editable, setEditable] = useState(false)
     const [data, setData] = useState({
         id: 0,
         name: "",
@@ -21,25 +22,54 @@ const TodoList = ({ todos }: TodoProps) => {
     const AddTodoOnClick = () => {
         dispatch(addTodo(data))
         dispatch(refreshTodo)
+        setData({ ...data, id: 0, name: "" })
     }
     const DeleteTodo = (id: number) => {
         dispatch(deleteTodo(id))
     }
+    const editData = (id: number, name: string) => {
+        setEditable(true)
+        setData({ ...data, id: id, name: name })
+    }
+    const CancelEdit = () => {
+        setEditable(false)
+        setData({ ...data, id: 0, name: "" })
+    }
     return (
-        <div>
+        <div className="mt-2">
             <button onClick={handleClick}>Refresh</button>
-            <div>
-                <input name="name" onChange={handleOnChange} />
-                <button onClick={AddTodoOnClick}>ADD TODO</button>
+            <div className="row m-2">
+                <input className="col form-control" name="name" onChange={handleOnChange} value={data.name} />
+                {editable ? (
+                    <button className="btn btn-warning mx-2">Update</button>
+                ) : (
+                    <button className="btn btn-primary mx-2" onClick={AddTodoOnClick}>
+                        ADD TODO
+                    </button>
+                )}
+
+                {editable ? (
+                    <button className="btn btn-danger mx-2" onClick={CancelEdit}>
+                        Cancel
+                    </button>
+                ) : (
+                    ""
+                )}
             </div>
-            <ul>
-                {todos.map((todos: Todo) => (
-                    <li key={todos.id}>
-                        {todos.name}
-                        <button onClick={() => DeleteTodo(todos.id)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+            {todos.map((todos: Todo) => (
+                <div key={todos.id} className="row m-3">
+                    <div className="col">
+                        <h4>{todos.name}</h4>
+                    </div>
+                    <div className="col">Completed</div>
+                    <button className="btn btn-primary mx-1" onClick={() => editData(todos.id, todos.name)}>
+                        Edit
+                    </button>
+                    <button className="btn btn-danger mx-1" onClick={() => DeleteTodo(todos.id)}>
+                        Delete
+                    </button>
+                </div>
+            ))}
         </div>
     )
 }
